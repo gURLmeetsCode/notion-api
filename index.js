14bec5f9b4d56a7355a442c185c14a246ede35f8
Natasha Kelly <natashakelly@mac-mini.myfiosgateway.com>
2021-09-23 17:09:38 -0400

    done

--- index.js
+++ index.js
@@ -4,13 +4,42 @@ let cors = require('cors')
 let router = express.Router()
 let app = express();
 
+app.use(express.json())
+
+let storage = []
+let chats = {}
+let users = []
+
+// chats = {"Eng" : [Amy, Tom]}
 
 app.get('/', (req, res) => {
   res.send("Hello World")
 });
 
+// This is sending messages to a chat
+app.post('/chat', (req, res) => {
+   storage.push(req.body)
+   console.log(storage)
+   res.status(201).json(req.body)
+})
+
+// This is joining a chat & supports multiple chats
+app.post('/join/:id', (req, res) => {
+let id = req.params.id
+let joinedChat = chats[id]
+console.log(id, users, chats)
+if (joinedChat){
+   chats[id].push(req.body.username)
+   res.send("You have joined the chat")
+}
+else{
+   chats[id] = [].push(req.body.username)
+   res.send("You have joined the chat")
+}
+})
 
 
+// This is handling multiple users in a chat
 
 
 app.listen(3000, () => {

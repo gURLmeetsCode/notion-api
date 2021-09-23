5ce5688ef210bdc2953a215cfe18ec95c7279ef4
Natasha Kelly <natashakelly@mac-mini.myfiosgateway.com>
2021-09-23 15:51:15 -0400

    initial

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 let express = require('express');
+let cookieSession = require('cookie-session')
+let cors = require('cors')
+let router = express.Router()
 let app = express();
 
-app.get('/', function(req, res){
-   res.send("Hello world! This is Natasha");
+
+app.get('/', (req, res) => {
+  res.send("Hello World")
 });
 
-app.listen(3000);
+
+
+
+
+app.listen(3000, () => {
+   console.log("Listening at http://localhost:3000")
+});
